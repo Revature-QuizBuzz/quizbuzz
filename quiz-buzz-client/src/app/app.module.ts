ed0dde302b65f6f45ad64f8e80b117093240afed
johnm0724 <36236210+johnm0724@users.noreply.github.com>
2021-09-09 11:08:07 -0400

    fix some bugs

--- quiz-buzz-client/src/app/app.module.ts
+++ quiz-buzz-client/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TaketestComponent } from './taketest/taketest.component';
+
+
+const routes:Routes = []
 
 @NgModule({
   declarations: [
