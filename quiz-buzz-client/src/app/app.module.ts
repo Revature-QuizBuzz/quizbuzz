987e7741d18d9fe21258402157711f6ab6762590
Anewsome09 <anthoney.newsome@revature.net>
2021-09-09 15:54:23 -0400

    Corrected Routing

--- quiz-buzz-client/src/app/app.module.ts
+++ quiz-buzz-client/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { TaketestComponent } from './taketest/taketest.component';
-import { QuizSubmissionComponent } from './quiz-submission/quiz-submission.component';
-import { QuizSelectionComponent } from './quiz-selection/quiz-selection.component';
+import { TaketestComponent } from './take.quiz.components/taketest/taketest.component';
+import { QuizSubmissionComponent } from './take.quiz.components/quiz-submission/quiz-submission.component';
+import { QuizSelectionComponent} from './take.quiz.components/quiz-selection/quiz-selection.component';
 
 @NgModule({
   declarations: [
