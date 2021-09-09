4143fac8c36f1097b0df9e4b617eaf2d459c5d80
Averonda <rearden@live.com>
2021-09-09 14:57:23 -0600

    added component

---
+++ quiz-buzz-client/src/app/create-quizes/create-quizes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-quizes',
+  templateUrl: './create-quizes.component.html',
+  styleUrls: ['./create-quizes.component.css']
+})
+export class CreateQuizesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
