d7594763c49cddd3e0adb91967b81faf76565228
alee-revature <angela.lee@revature.net>
2021-09-09 12:37:45 -0400

    adding overall ui and login html/css/bootstrap

--- quiz-buzz-client/src/app/app-routing.module.ts
+++ quiz-buzz-client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', redirectTo: '/home', component: HomeComponent},
+  {path: '/login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
