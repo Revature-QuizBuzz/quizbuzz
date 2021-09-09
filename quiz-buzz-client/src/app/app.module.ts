5c989b292205e633bbb61dc3e92a4c9d1e9cc6f9
Smitty0078 <smitty0078@gmail.com>
2021-09-09 11:36:31 -0400

    Added quiz taking components

--- quiz-buzz-client/src/app/app.module.ts
+++ quiz-buzz-client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TaketestComponent } from './taketest/taketest.component';
+import { QuizSubmissionComponent } from './quiz-submission/quiz-submission.component';
+import { QuizSelectionComponent } from './quiz-selection/quiz-selection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TaketestComponent,
+    QuizSubmissionComponent,
+    QuizSelectionComponent
   ],
   imports: [
     BrowserModule,
