7f666cf7d96b167ca0dc4d6fa55cf6af027f207c
Aivansky <51238426+Aivansky@users.noreply.github.com>
2021-09-09 11:28:35 -0700

    adding rudimentary Junit test

--- back_end/quizbuzz/src/test/java/quiz/QuizbuzzApplicationTests.java
+++ back_end/quizbuzz/src/test/java/quiz/QuizbuzzApplicationTests.java
@@ -2,12 +2,23 @@ package quiz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import quiz.services.QuizManager;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class QuizbuzzApplicationTests {
+	
+	 @Autowired
+	 private QuizManager manager;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void test2() throws Exception {
+		assertThat(manager).isNotNull();
+	    System.out.println("quizmanager is not null");
 	}
 
 }
