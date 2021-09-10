6fa7d854748b366549bfa908427e057dea315c24
Jesse Lauesen <jesse.lauesen@revature.net>
2021-09-10 10:27:00 -0500

    angular testing

--- quiz-buzz-client/src/app/new-tags/new-tags.component.spec.ts
+++ quiz-buzz-client/src/app/new-tags/new-tags.component.spec.ts
@@ -1,11 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms'; 
 
 import { NewTagsComponent } from './new-tags.component';
 
 describe('NewTagsComponent', () => {
   let component: NewTagsComponent;
   let fixture: ComponentFixture<NewTagsComponent>;
-
+  const testForm = <NgForm>{
+    name: "Not Java"
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ NewTagsComponent ]
@@ -22,4 +25,9 @@ describe('NewTagsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return null',()=>{
+    expect(component.onSubmit(testForm)).toBe();
+  });
+  
 });
