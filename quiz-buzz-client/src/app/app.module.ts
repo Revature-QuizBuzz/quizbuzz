17114d43545ba1579915bf0a3004722ba5f66f7b
SeraphimThomas <foxboyseraphim@gmail.com>
2021-09-10 15:19:20 -0500

    Angular updates
    Just angular changes

--- quiz-buzz-client/src/app/app.module.ts
+++ quiz-buzz-client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { QuestionAnswerComponent } from './question-answer/question-answer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { QuestionAnswerComponent } from './question-answer/question-answer.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
