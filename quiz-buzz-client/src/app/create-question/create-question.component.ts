a951c8f1993482903023ba8aeb12c0d762e37759
e-Henderson <eddie.henderson@revature.net>
2021-09-13 15:57:26 -0400

    minor changes

--- quiz-buzz-client/src/app/create-question/create-question.component.ts
+++ quiz-buzz-client/src/app/create-question/create-question.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { NgForm } from '@angular/forms';
+import { Question } from '../models/questions';
 
 @Component({
   selector: 'app-create-question',
@@ -9,10 +10,29 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-question.component.css']
 })
 export class CreateQuestionComponent implements OnInit {
+  //Http: any;//shouldn't need this but whatever
 
-  constructor(Http: HttpClient, 
+  constructor(private Http: HttpClient, 
     private router: Router) { }
 
+  onSubmit(form: NgForm){
+    console.log(form);
+    this.Http.post("http://localhost:8080/questions/createquestion",
+    {
+      question: form.value.question,
+      type: form.value.type,
+      answers: form.value.answers,
+      possiblePoints: form.value.possiblePoints
+    })
+    .subscribe({
+      next: (data:any)=>{
+        //this.router.navigate(['home']);
+      },
+      //error: (error)=>{
+        //console.log(error);
+      //},complete: ()=>{}
+    })
+  }
     
 
   ngOnInit(): void {
