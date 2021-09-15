a0ad617d31e121b1233beda8b5cdb54140e26137
Smitty0078 <smitty0078@gmail.com>
2021-09-15 12:14:45 -0400

    created routs for take quiz components

--- quiz-buzz-client/src/app/app-routing.module.ts
+++ quiz-buzz-client/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { LoginComponent } from './login/login.component';
 import { NewTagsComponent } from './new-tags/new-tags.component';
 import { QuizSubmissionComponent } from './take-quiz-components/quiz-submission/quiz-submission.component';
 import { CreateQuizesComponent } from './create-quizes/create-quizes.component';
+import { QuizSelectionComponent } from './take-quiz-components/quiz-selection/quiz-selection.component';
+import { TaketestComponent } from './take-quiz-components/taketest/taketest.component';
 
 
 export const routes: Routes = [
@@ -14,7 +16,10 @@ export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'testresults', component: QuizSubmissionComponent},
   {path:"tags/new", component: NewTagsComponent},
-  {path: 'quiz/new', component:CreateQuizesComponent}
+  {path: 'quiz/new', component:CreateQuizesComponent},
+  {path: '/quiz/selection', component: QuizSelectionComponent},
+  {path: '/take/quiz', component: TaketestComponent},
+  {path: '/quiz/submit', component: QuizSubmissionComponent}
 ];
 
 @NgModule({
