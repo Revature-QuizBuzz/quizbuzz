a0ca85156d6710d4f4b55a639d1a6fac264c3de5
Smitty0078 <smitty0078@gmail.com>
2021-09-29 11:01:15 -0400

    fixed spacing and styling in quiz selection

--- back_end/quizbuzz/src/main/java/quiz/dao/QuizDAO.java
+++ back_end/quizbuzz/src/main/java/quiz/dao/QuizDAO.java
@@ -33,6 +33,7 @@ public interface QuizDAO extends JpaRepository<Quiz, Integer> {
     @Query(value="select * from quizbuzz.quizzes limit 5", nativeQuery=true)
     public List<Quiz> getFeaturedQuizzes();
     
-    @Query(value = "SELECT * FROM quizbuzz.quizzes WHERE name LIKE :name%", nativeQuery = true)
+    //@Query(value = "SELECT * FROM quizbuzz.quizzes WHERE name LIKE :name%", nativeQuery = true)
+    @Query(value = "SELECT distinct quizzes.quiz_id, quizzes.user_id, quizzes.name, quizzes.description, quizzes.total_score, quizzes.created_date, quizzes.date_modified FROM quizbuzz.quizzes LEFT JOIN quizbuzz.quiz_tags ON quiz_tags.quiz_id = quizzes.quiz_id INNER JOIN quizbuzz.tags ON tags.tag_id = quiz_tags.tag_id and tags.name like :name% or quizzes.name like :name%", nativeQuery = true)
 	List<Quiz> findByQuizName(@Param("name") String quizName);
 }
